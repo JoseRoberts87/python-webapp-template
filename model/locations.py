6c41fa88dfe01ea2ecaa436685e73702acc12519
Jose Roberts <robenjos@amazon.com>
2018-06-04 11:02:55 -0400

    saving chnages to locations model

--- model/locations.py
+++ model/locations.py
@@ -1,9 +1,9 @@
 from app import db, ma
 from common import utils
 
-class Locations(db.Model):
 
-    __table__ = 'arde_locations'
+class Locations(db.Model):
+    __tablename__ = 'arde_locations'
 
     location_id = db.Column(db.String(45), primary_key=True)
     name = db.Column(db.String(45))
@@ -11,9 +11,11 @@ class Locations(db.Model):
     level = db.Column(db.Integer)
     status = db.Column(db.String(45))
 
+
 class LocationsSchema(ma.Schema):
     class Meta(object):
         fields = utils.class_attributes(Locations)
+
 
 LOCATION_SCHEMA = LocationsSchema()
 LOCATIONS_SCHEMA = LocationsSchema(many=True)
